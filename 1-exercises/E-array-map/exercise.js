cdfeb8437107528e8f8b29eb369234883726726d
konikalily <konika.l.biswas@gmail.com>
2021-07-09 17:12:51 +0100

    E-array-map multiple var declaration commented out

--- 1-exercises/E-array-map/exercise.js
+++ 1-exercises/E-array-map/exercise.js
@@ -11,17 +11,17 @@ const numbersMultiplied = numbers.map(multipliedArray);
 
 //declaring  the callback function inside of the method call
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+//var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 const numbersMultiplied1 = numbers.map(function multipliedArray(number) {
   return number * 100;
 });
 //using map()without function name
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+//var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 const numbersMultiplied2 = numbers.map(function (number) {
   return number * 100;
 });
 // using arrow function
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+//var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
 const numbersMultiplied3 = numbers.map((number) => number * 100);
 console.log(numbersMultiplied);
 console.log(numbersMultiplied1);
